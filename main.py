db67b7bb1815131961a54213ffce6e29094bd35b
peterstrongg <peterstrongdev@outlook.com>
2024-03-10 12:58:26 -0400

    API Route defined

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ from flask import (
 )
 from flask_cors import CORS
 
+from outlook import outlook
+
 service = Flask(__name__)
 
-
+@service.route("/api/v1/parsemsg", methods=["GET"])
+def msgfile():
+    o = outlook.OutlookMsg("./data/ap.msg")
+    data = {
+        "header" : o.get_header(),
+        "sender" : o.get_sender(),
+        "date" : o.get_date(),
+        "subject" : o.get_subject(),
+        "body" : o.get_body()
+    }
+    return jsonify(data)
 
 if __name__ == "__main__":
     service.run()
--- outlook/outlook.py
+++ outlook/outlook.py
@@ -4,4 +4,25 @@ class OutlookMsg:
     def __init__(self, file_path):
         self.file = extract_msg.openMsg(file_path)
 
+        self.header = self.file.header
+        self.sender = self.file.sender
+        self.date = self.file.date
+        self.subject = self.file.subject
+        self.body = self.file.body
+
+    def get_header(self):
+        return str(self.header)
+
+    def get_sender(self):
+        return str(self.sender)
+    
+    def get_date(self):
+        return str(self.date)
+    
+    def get_subject(self):
+        return str(self.subject)
+
+    def get_body(self):
+        return str(self.body)
+
 o = OutlookMsg("./data/ap.msg")
